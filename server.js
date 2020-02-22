3b16f6e46d957ebb78142e0d69f995b377afbe3b
chukwiz <chukswiz@gmail.com>
2020-02-22 12:49:00 +0100

    new commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 4000;
+const path = require('path');
+const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern', {useNewUrlParser:true});
+
+app.use(cors({
+    origin: process.env.ALLOW_ORIGIN,
+    credentials: true,
+    allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
+    methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS'
+  }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+
+let Users = require('./routes/Users');
+app.use('/users',Users)
+
+if(process.env.NODE_ENV === "production"){
+    app.disable('x-powered-by')
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname, 'client','build','index.html'));
+    })
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
