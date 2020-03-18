a9ea94b15a914954d160c0ade5cc206fdef6ecd2
chukwiz <chukswiz@gmail.com>
2020-03-18 14:34:09 +0100

    build

---
+++ client/build/precache-manifest.53017b1930024674ea8bb05266a97587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52b8850649f656e38357c88ea923392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629e3d274eba01a631ad",
+    "url": "/static/css/2.57944c3f.chunk.css"
+  },
+  {
+    "revision": "fc4b639edaa707fc3be8",
+    "url": "/static/css/main.de24c4a3.chunk.css"
+  },
+  {
+    "revision": "629e3d274eba01a631ad",
+    "url": "/static/js/2.a2b445e9.chunk.js"
+  },
+  {
+    "revision": "d1a971c7e2f2ad8045f0adae13db4dba",
+    "url": "/static/js/2.a2b445e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc4b639edaa707fc3be8",
+    "url": "/static/js/main.e27768fe.chunk.js"
+  },
+  {
+    "revision": "0c29700605f72fe4f2d8",
+    "url": "/static/js/runtime-main.8886d7c2.js"
+  }
+]);
